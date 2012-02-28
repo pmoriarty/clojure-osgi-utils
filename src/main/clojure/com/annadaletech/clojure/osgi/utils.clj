e0a4854c3bf9eea027f178d94ccff20f733a186a
Paudi Moriarty <pmoriarty@annadaletech.com>
2012-02-28 15:23:28 +0000

    Initial commit of clojure-osgi-utils

---
+++ src/main/clojure/com/annadaletech/clojure/osgi/utils.clj
@@ -0,0 +1,60 @@
+(ns com.annadaletech.clojure.osgi.utils
+  (:use
+    clojure.java.io
+    clojure.tools.namespace
+    ))
+
+(defn get-ns-libs
+  "Returns a seq of libs of the given type (:use, :require, :import) from the namespace declaration."
+  [type ns]
+  (rest (first (filter #(and (sequential? %1) (= type (first %1))) ns))))
+
+(defn get-ns-name
+  "Returns the name from the given namespace declaration."
+  [ns]
+  (second ns))
+
+(defn get-package
+  "Returns the package (in Java terms) as a String for a given reference (libspec, prefix list or flag).
+  The libspec may be a symbol or a sequential.
+    symbol:
+    x.y.z => x.y
+    sequence:
+    (x.y z1 z2) => x.y
+    [x.y.z :as zz] => x.y"
+  [reference]
+  (letfn [(get-base [package-and-class]
+            (let [package-and-class (name package-and-class)
+                  [_ package class] (re-find #"(.*)\.(.*)" package-and-class)]
+              package))]
+    (cond
+      ;libspecs
+      (symbol? reference) (get-base reference)
+      ;prefix lists
+      (sequential? reference) (if (keyword? (second reference)) (get-base (first reference))
+                                                                (name (first reference)))
+      ;flags
+      (keyword? reference) nil)))
+
+(defn find-missing-imports
+  "Finds packages in use/require/import declarations in clojure source files in base-dir
+   and checks that all are present in the manifest file.
+   Takes seq of ignored-packages (Strings) to exclude certain packages from consideration.
+   Also excludes any packages provided by the clojure source files in base-dir.
+   "
+  [base-dir manifest ignored-packages]
+  (let [ignored-packages (into #{} ignored-packages)
+        ns-decls (find-ns-decls-in-dir base-dir)
+        declared-packages (into #{} (map (comp get-package get-ns-name) ns-decls))
+        uses (reduce concat [] (map (partial get-ns-libs :use) ns-decls))
+        used-packages (into #{} (map get-package uses))
+        requires (reduce concat [] (map (partial get-ns-libs :require) ns-decls))
+        required-packages (into #{} (map get-package requires))
+        imports (reduce concat [] (map (partial get-ns-libs :import) ns-decls))
+        imported-packages (into #{} (map get-package imports))]
+    (for [ns (concat used-packages required-packages imported-packages)
+          :when (and ns
+                     (not (declared-packages ns))
+                     (not (ignored-packages ns))
+                     (not (.contains manifest ns)))]
+      ns)))
---
+++ src/test/clojure/com/annadaletech/clojure/osgi/utils_test.clj
@@ -0,0 +1,59 @@
+(ns com.annadaletech.clojure.osgi.utils-test
+  (:use clojure.test
+        clojure.tools.namespace
+        com.annadaletech.clojure.osgi.utils
+        )
+  )
+
+(deftest get-ns-libs-test
+  (let [decl '(ns dummy (:use a b [c d]) (:require x [y.z :as zz]))]
+    (is (= '(a b [c d]) (get-ns-libs :use decl)))
+    (is (= '(x [y.z :as zz]) (get-ns-libs :require decl)))
+    (is (= '() (get-ns-libs :import decl)))
+    )
+  (testing "with doc string"
+    (let [decl '(ns dummy "doc-string" (:use a b [c d]) (:require x [y.z :as zz]))]
+      (is (= '(a b [c d]) (get-ns-libs :use decl)))
+      (is (= '(x [y.z :as zz]) (get-ns-libs :require decl)))
+      (is (= '() (get-ns-libs :import decl)))
+      )
+    )
+  )
+
+(deftest get-ns-name-test
+  (let [decl '(ns dummy (:use a b [c d]) (:require x [y.z :as zz]))]
+    (is (= 'dummy (get-ns-name decl)))
+    )
+  )
+
+(deftest get-package-tests
+  (testing "libspec"
+    (is (= "x.y" (get-package 'x.y.z)))
+    (is (nil? (get-package 'x)))
+    (is (= "x" (get-package '[x.y :as z])))
+    )
+  (testing "prefix lists"
+    (is (= "x.y" (get-package '[x.y z])))
+    (is (= "x" (get-package '[x y1 [y2 :as z]])))
+    )
+  (testing "flags are ignored"
+    (is (nil? (get-package :reload)))
+    )
+  )
+
+(deftest find-missing-imports-tests
+  (with-redefs [find-ns-decls-in-dir
+                (fn [dir]
+                  '((ns ns1 (:use a1.b1 [c1 d]) (:require x1 [y1.z :as zz]) (:import d1.e1.f))
+                     (ns ns2 (:use a2.b2 [c2 d]) (:require x2 [y2.z :as zz]) (:import d2.e2.f))
+                     ))]
+    (let [base-dir 'dummy-dir
+          manifest ""
+          ignored-packages []]
+      (is (= ["c1" "c2" "a1" "a2" "y1" "y2" "d1.e1" "d2.e2"] (find-missing-imports base-dir manifest ignored-packages)))
+      )))
+
+
+; Uncomment to run these tests in IDEA, recomment again as clojure-maven-plugin does not need this
+(run-tests 'com.annadaletech.clojure.osgi.utils-test)
+
